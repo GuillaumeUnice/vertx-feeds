b1f6e86890b7f0912e0ddb5b87d104f9ca695a93
GuillaumeUnice <guillaume.unice@gmail.com>
2017-03-21 14:36:42 +0100

    remove useless file + add npm dependency

--- webroot/scripts/angular-interpolator-shim.js
+++
@@ -1,8 +0,0 @@
-window.vertxFeeds = angular.module('vertxFeeds', ['ngMaterial', 'mdColorPicker']);
-
-vertxFeeds.config(function($interpolateProvider, $mdThemingProvider){
-    $mdThemingProvider.theme('default')
-        .primaryPalette('blue')
-        .accentPalette('orange');
-    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-});
